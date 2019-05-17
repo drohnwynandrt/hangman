93e19f25a715c383a8fa4d9f09f6023408f68f28
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-05-17 10:13:29 +0200

    the loop will now assign an id to each div (letter)

--- hangman.js
+++ hangman.js
@@ -3,6 +3,7 @@ var word = ["t", "e", "s", "t"]
 function writeWord(){
     for (var i=0; i < word.length; i++){
         var newDiv = document.createElement('div');
+        newDiv.id= "letter"+i;
         newDiv.textContent = "_";
 
         var divParent = document.getElementById("theWord");
@@ -14,7 +15,7 @@ function checkWord(){
     if (document.getElementById("guess").value== word){
     alert("Goedzo!");}
     else {
-        alert("Bijna goed!");}
+        alert("Jammer... Probeer het nog eens!");}
 }
 
 function checkLetter(){
